515581dcc9d152f120c350525f943b645b5a3cd7
Garrett Hargrove <ghargrove15@gmail.com>
2022-06-24 21:33:51 -0500

    Added video

---
+++ Modules.TheBedroom.js
@@ -0,0 +1,11 @@
+async function btnPlayButton_onClick() {
+   document.getElementById("btnPlayButton").style.opacity = 0;
+   
+   await new Promise(r => setTimeout(r, 3000));
+   document.getElementById("btnPlayButton").style.display = "none";
+   document.getElementById("vidCridimal").style.display = "inline";
+   await new Promise(r => setTimeout(r, 10));
+
+   document.getElementById("vidCridimal").style.opacity = 100;
+   document.getElementById("vidCridimal").play();
+}
