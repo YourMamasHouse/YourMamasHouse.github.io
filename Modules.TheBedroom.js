0392b51c81511a6512199378d35065142fc40a54
Garrett Hargrove <ghargrove15@gmail.com>
2022-06-26 00:28:24 -0500

    Finished

--- Modules.TheBedroom.js
+++ Modules.TheBedroom.js
@@ -38,6 +38,27 @@ async function btnAlertButton_onClick() {
     document.getElementsByClassName("walterLeft")[0].classList.add("visible");
 
     document.getElementById("vidCridimal").play();
+
+    // Move to shop
+    await new Promise(r => setTimeout(r, 15000));
+    await transitionSection("vwVideo", "vwShop");
+    setUpShop();
 }
 
+function setUpShop()
+{
+    document.getElementById("imgSuperLaserPiss").addEventListener("click", () => openTab("Ohio-Piss-by-ghargrove15/114865771.WFLAH"));
+    document.getElementById("imgWhyCridimal").addEventListener("click", () => openTab("Why-you-a-cridimal-by-ghargrove15/114846598.WFLAH"));
+    document.getElementById("imgCridimal").addEventListener("click", () => openTab("Cridimal-by-ghargrove15/114846822.WFLAH"));
+    document.getElementById("imgWalterShirt").addEventListener("click", () => openTab("Walter-Cridimal-by-ghargrove15/114846382.WFLAH"));
+    document.getElementById("imgKanye").addEventListener("click", () => openTab("Kanye-by-ghargrove15/114847213.WFLAH"));
+}
+
+function openTab(itemName)
+{
+    var url = "https://www.redbubble.com/i/t-shirt/" + itemName;
+
+    window.open(
+        url, "_blank");
+}
 //#endregion
